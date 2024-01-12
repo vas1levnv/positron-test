8a43a4ffea7f8567fc4a69da4ea2978002ebdd8f
vas1levnv <vas1levnv@yandex.ru>
2024-01-12 07:24:01 +0300

    Верстает свайпер просмотренных товаров

---
+++ components/icons/IconSwiper.vue
@@ -0,0 +1,6 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+		<circle cx="25" cy="25" r="25" transform="rotate(-180 25 25)" fill="#90A2B5"/>
+		<path d="M28 34L20 25L28 16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+	</svg>
+</template>
--- components/salesSwiper.vue
+++ components/salesSwiper.vue
@@ -1,12 +1,15 @@
 <template>
 	<div class="sales-swiper">
-		<h2>Просмотренные товары</h2>
+		<h2 class="sales-swiper-title">Просмотренные товары</h2>
 		<swiper
 				:slides-per-view="4"
 				:space-between="20"
+				:navigation="{
+      prevEl: prev,
+      nextEl: next,
+    }"
 				:modules="modules"
-				navigation
-				:pagination="{ clickable: true }"
+				:pagination="{ clickable: true,type: 'fraction', el: pagination, }"
 		>
 			<swiper-slide class="sales-swiper-item" v-for="item in store.salesSwiper">
 				<div>
@@ -16,17 +19,29 @@
 					<div>{{ item.name }}</div>
 					<div>{{ item.description }}</div>
 				</div>
-				<div style="font-weight: 700; font-size: 1.5rem">{{ item.priceRu.first }} ₽ - {{ item.priceRu.second }} ₽</div>
+				<div style="font-weight: 700; font-size: 1.5rem">{{ item.priceRu.first }} ₽ - {{ item.priceRu.second }}
+					₽
+				</div>
 				<div>{{ item.priceEu.first }} € - {{ item.priceEu.second }} €</div>
 				<custom-button class="primary">Подробнее</custom-button>
 			</swiper-slide>
+			<div class="sales-swiper-others">
+				<div ref="prev" class="prev">
+					<icon-swiper/>
+				</div>
+
+				<div ref="pagination"></div>
+				<div ref="next" class="next">
+					<icon-swiper/>
+				</div>
+			</div>
 		</swiper>
 	</div>
 </template>
 
 <script setup lang="ts">
 import {Swiper, SwiperSlide} from 'swiper/vue';
-import { Navigation, Pagination} from 'swiper/modules';
+import {Navigation, Pagination} from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -34,14 +49,32 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import {useWebsiteStore} from "~/stores/indexStore";
 import noPhotoSwiper from "../assets/img/no-photo-swiper.png"
+import {ref} from 'vue';
+import IconSwiper from "~/components/icons/IconSwiper.vue";
 
 const store = useWebsiteStore()
 const modules = [Pagination, Navigation]
+
+const prev = ref(null)
+const next = ref(null)
+const pagination = ref(null)
+
 </script>
 
 <style scoped lang="scss">
 .sales-swiper {
 	margin-top: 3rem;
+	position: relative;
+	
+	&-title {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	
+	.swiper {
+		padding-top: 4rem;
+	}
 	
 	&-item {
 		display: flex;
@@ -52,10 +85,32 @@ const modules = [Pagination, Navigation]
 		padding: 1.5rem;
 		height: auto !important;
 		
-		&__text{
+		&__text {
 			flex: 1 1 0;
 			margin-bottom: 2.25rem;
 		}
 	}
+	
+	&-others {
+		position: absolute;
+		top: 0;
+		right: 0;
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		z-index: 1000;
+		
+		.next, .prev{
+			width: 50px;
+			height: 50px;
+			cursor: pointer;
+		}
+		
+		.next{
+			transform: rotate(180deg);
+		}
+	}
 }
+
+
 </style>
